6357136aa29daca7d2332bd77387fa63e84b7090
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-11-09 22:30:21 +0100

    Updated Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
     <header className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${isScrolled ? 'bg-abaris-cyan' : ''}`}>
       <div className="max-w-7xl mx-auto px-2 md:px-3 py-2 md:py-2">
         <div className="flex items-center justify-between">
-          <div className="text-white text-2xl md:text-3xl font-light tracking-wide cursor-pointer leading-none" onClick={() => scrollToSection('hero')}>
+          <div className="text-white text-2xl md:text-3xl font-black tracking-wide cursor-pointer leading-none" onClick={() => scrollToSection('hero')}>
             Abaris
           </div>
 
