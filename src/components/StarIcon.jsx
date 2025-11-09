cbc15c87b6d2b53eaa83951d1522ca72997020c6
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-11-09 17:35:36 +0100

    Updated StarIcon.jsx

--- src/components/StarIcon.jsx
+++ src/components/StarIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function StarIcon() {
   return (
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" className="text-white">
+    <svg width="20" height="20" viewBox="0 0 200 200" fill="none" className="text-white">
       <path
-        d="M12 2 L13.5 10.5 L22 12 L13.5 13.5 L12 22 L10.5 13.5 L2 12 L10.5 10.5 Z"
+        d="M100 10 L105 90 L185 95 L115 145 L145 200 L100 150 L55 200 L85 145 L15 95 L95 90 Z"
         fill="currentColor"
       />
     </svg>
