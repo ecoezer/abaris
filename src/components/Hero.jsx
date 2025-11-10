0ca3157ba3f22eb82aef657515df755a4ff38357
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-11-10 10:20:35 +0100

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -71,7 +71,7 @@ und Eleganz, die bleibt.
                   <div className="mb-1 md:mb-1 lg:mb-2 flex items-center justify-center w-10 md:w-10 lg:w-12 h-10 md:h-10 lg:h-12 rounded-full border-2 border-orange-500 transition-all duration-300 group-hover:scale-110 group-hover:bg-orange-500 group-hover:bg-opacity-20">
                     <GoNorthStar className="text-orange-500" size={13} />
                   </div>
-                  <h3 className="text-white text-lg md:text-base lg:text-2xl font-light mb-1 md:mb-1 lg:mb-2 transition-all duration-300 group-hover:scale-105">Abaris Nachhaltigkeit – Sauberkeit mit Verantwortung</h3>
+                  <h3 className="text-white text-lg md:text-base lg:text-[1.3rem] font-light mb-1 md:mb-1 lg:mb-2 transition-all duration-300 group-hover:scale-105">Abaris Nachhaltigkeit – Sauberkeit mit Verantwortung</h3>
                   <p className="text-white font-light text-xs md:text-xs lg:text-sm leading-relaxed opacity-90">
                     Schonend für Ihre Räume. Schonend für unsere Umwelt.
                   </p>
