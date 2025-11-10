6305ea333bd521c2dfbd2d650ec049b4515c120f
Emrah C. Oezer <58663590+ecoezer@users.noreply.github.com>
2025-11-10 10:37:33 +0100

    Updated Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ function Services() {
   const services = [
     {
       title: 'Abaris Cicero Service - Büroreinigung',
-      description: 'Wir garantieren eine regelmäßige und bedarfsgerechte Reinigung Ihrer Büro- und Sozialräume nach einem individuell erstellten Reinigungsplan. Unser Team reinigt effektiv Ihre Sanitärbereiche und desinfiziert Türgriffe u.a. standardmäßig.\n\nEgal welchen Boden Sie haben, Teppich, Fliesen, Stein oder Parkett – Für uns kein Problem! Wir übernehmen die fachgerechte Reinigung.'
+      description: 'Wo Raum und Seele im Gleichklang sind\nUnsere Umgebung hat einen starken Einfluss auf unsere Stimmung und Leistungsfähigkeit.\nEin sauberer, gepflegter Raum schafft Ruhe, Klarheit und positive Energie.\nGerade im Büro, wo wir oft mehr Zeit verbringen als zu Hause, ist eine saubere Umgebung der Schlüssel für konzentriertes und harmonisches Arbeiten.\nEin hygienisches und ordentliches Büro fördert nicht nur die Produktivität, sondern auch das innere Gleichgewicht.\nDenn Sauberkeit bedeutet mehr als Ordnung – sie schenkt Leichtigkeit für Körper, Geist und Seele. Wo Raum und Seele im Gleichklang sind.'
     },
     {
       title: 'Abaris Kitas Service',
